38bd4d915c2d0541e939394db5f83c37a8abeb25
ajharul <mdajharulislam327@gmail.com>
2025-10-26 00:23:10 +0600

    currection

--- fetchData1.js
+++ fetchData1.js
@@ -1,30 +1,25 @@
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 const Component = () => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
 
-const fetchData = async ()=>{
-
+  const fetchData = async () => {
     try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
 
-        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-
-        const data1 = await response.json();
-        setData(data1)
-
+      const data1 = await response.json();
+      setData(data1);
     } catch (error) {
-        console.error(error.message)
+      console.error(error.message);
     }
-}
-useEffect(()=>{
+  };
+  useEffect(() => {
     fetchData();
-},[])
+  }, []);
 
-  return (
-    <div>
+  return <div></div>;
+};
 
-    </div>
-  )
-}
-
-export default Component
+export default Component;
