050398a3b035d9d26bd89cc36fad5dea9eb5ecd5
ajharul <mdajharulislam327@gmail.com>
2025-10-27 00:45:32 +0600

    add db connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import express, { urlencoded } from 'express'
 import homeRoute from './routes/homeRoute.js';
 import cors from 'cors'
+import connectDB from './config/db.js';
 const app = express();
 
 
 const PORT = 4000;
-
+//await connectDB();
 app.use(express.json())
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
