0732c228da2ea11837e86d6e37a4fc14ab91b3a8
ajharul <mdajharulislam327@gmail.com>
2025-10-28 00:28:07 +0600

    add User controller codes

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/User";
+import bcrypt from 'bcrypt'
 
 export const register = async(req, res)=>{
     try {
@@ -13,7 +14,30 @@ export const register = async(req, res)=>{
         }
 
         const exestingUser = await User.findOne({email})
+        if(exestingUser){
+            return res.status(400).json({
+                success:false,
+                message:"User Already exist"
+            })
+        }
+        const hashPass = await bcrypt.hash(password, 10)
+
+        const user = await User.create({
+            name,
+            email,
+            password:hashPass
+        })
+
+       return res.status(201).json({
+        user,
+                success:true,
+                message:"User created successfully"
+        })
     } catch (error) {
         console.error(error.message)
+        return res.status(400).json({
+            success:false,
+            message:"Internal server error"
+        })
     }
 }
